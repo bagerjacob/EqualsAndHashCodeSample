79e8406bc37660d7e58ba007c4dcb51e4a35fefe
Jacob Bager <jacob.bager@bestseller.com>
2025-11-18 14:55:59 +0100

    Add @Transient annotation to version field and update dependencies in pom.xml

--- src/main/java/com/bestseller/bsone/bi4/sales/sandbox/model/MappedClass.java
+++ src/main/java/com/bestseller/bsone/bi4/sales/sandbox/model/MappedClass.java
@@ -1,11 +1,12 @@
 package com.bestseller.bsone.bi4.sales.sandbox.model;
 
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Data
 public class MappedClass
 {
-
+  @Transient
   private Long version;
 
 }
